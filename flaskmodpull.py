6da5418e2898c9329bd805712ff95a6fb781b71a
JoshA <isjav08@gmail.com>
2018-12-21 15:07:24 -0600

    Create flaskmodpull.py

---
+++ flaskmodpull.py
@@ -0,0 +1,34 @@
+import sys
+
+def modpull(a):
+    import requests
+    import sys
+    import json
+    URL = 'https://api.warframestat.us/mods'
+    r = requests.get(URL)
+
+    print("\n")
+    wf_objs = r.json()
+    for obj in wf_objs:
+        if obj['category'] == u'Mods' and a in obj['name'].lower() :
+            print ("Name: %s") % obj['name']
+            print ("Polarity: %s") % obj['polarity']
+            print ("Rarity: %s") % obj['rarity']
+            print ("Description: %s") % obj['description']
+
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "all":
+            for obj in wf_objs:
+                if obj['category'] == u'Mods' and a in obj['name'].lower() :
+                    print obj
+
+
+
+
+#def main():
+#  inputname = sys.argv[1].lower()
+#  x=modpull(inputname)
+
+#if __name__ == "__main__":
+#  main()
+modpull("wildfire")
