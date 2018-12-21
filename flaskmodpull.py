72907a3dda42ab1014a336d2ef77d134f1298e87
Wael ElMasry <wael.elmasry@live.com>
2018-12-21 15:32:06 -0600

    Update flaskmodpull.py

--- flaskmodpull.py
+++ flaskmodpull.py
@@ -1,20 +1,24 @@
+#!/usr/bin/python2
+
 
 import sys
-def modpull(a):
+def modpull(a, b):
     import requests
     import sys
     import json
     URL = 'https://api.warframestat.us/mods'
     r = requests.get(URL)
-    print("\n")
     wf_objs = r.json()
     for obj in wf_objs:
         if obj['category'] == u'Mods' and a in obj['name'].lower() :
-            print ("Name: %s") % obj['name']
-            print ("Polarity: %s") % obj['polarity']
-            print ("Rarity: %s") % obj['rarity']
-            print ("Description: %s") % obj['description']
-            return obj['name'] , obj['polarity']
+            if b == "name":
+              return "Name: %s" % obj['name']
+            elif b == "Polarity":
+              return ("Polarity: %s") % obj['polarity']
+            elif b == "Rarity":
+              return ("Rarity: %s") % obj['rarity']
+            elif b  == "Description":
+              return ("Description: %s") % obj['description']
     if len(sys.argv) > 2:
         if sys.argv[2] == "all":
             for obj in wf_objs:
@@ -22,7 +26,14 @@ def modpull(a):
                     print obj
 def main():
   inputname = sys.argv[1].lower()
-  x=modpull(inputname)
-  print x
-if __name__ == "__main__":
+  x=modpull(inputname, "name")
+  print (x)
+  x=modpull(inputname, "Polarity")
+  print(x)
+  x=modpull(inputname, "Rarity")
+  print(x)
+  x=modpull(inputname, "Description")
+  print(x)
+
+if __name__  == "__main__":
   main()
