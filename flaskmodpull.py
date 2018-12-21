9816c9ed54d2b302154c601d52511df88d45499d
JoshA <isjav08@gmail.com>
2018-12-21 15:15:55 -0600

    Testing

--- flaskmodpull.py
+++ flaskmodpull.py
@@ -1,12 +1,10 @@
 import sys
-
 def modpull(a):
     import requests
     import sys
     import json
     URL = 'https://api.warframestat.us/mods'
     r = requests.get(URL)
-
     print("\n")
     wf_objs = r.json()
     for obj in wf_objs:
@@ -15,20 +13,14 @@ def modpull(a):
             print ("Polarity: %s") % obj['polarity']
             print ("Rarity: %s") % obj['rarity']
             print ("Description: %s") % obj['description']
-
+            return obj['name'] , obj['polarity']
     if len(sys.argv) > 2:
         if sys.argv[2] == "all":
             for obj in wf_objs:
                 if obj['category'] == u'Mods' and a in obj['name'].lower() :
                     print obj
-
-
-
-
-#def main():
-#  inputname = sys.argv[1].lower()
-#  x=modpull(inputname)
-
-#if __name__ == "__main__":
-#  main()
-modpull("wildfire")
+def main():
+  inputname = sys.argv[1].lower()
+  x=modpull(inputname)
+  print x
+if __na
